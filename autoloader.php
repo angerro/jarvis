004740715e02bec411d37338564d34b720e0a969
angerro <anhel.spb@gmail.com>
2022-01-21 20:51:35 +0300

    fix autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -3,13 +3,15 @@
 use Jarvis\Vendor\Config;
 
 spl_autoload_register(function ($class) {
-    $pathParts = explode('\\', $class);
-    $pathParts[0] = strtolower($pathParts[0]);
-    $classPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $pathParts) . '.php';
-    if (file_exists($classPath)) {
-        require $classPath;
-    } else {
-        throw new \Exception("Ошибка загрузки '$classPath'");
+    if (substr($class, 0, 6) === "Jarvis"){
+        $pathParts = explode('\\', $class);
+        $pathParts[0] = strtolower($pathParts[0]);
+        $classPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $pathParts) . '.php';
+        if (file_exists($classPath)) {
+            require $classPath;
+        } else {
+            throw new \Exception("Ошибка загрузки '$classPath'");
+        }
     }
 });
 
